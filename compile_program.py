6ee26e7a63804c067ce7f3443b9f56a43334e180
SladetBask-Kasper <kasper4131@gmail.com>
2021-01-21 14:50:22 +0100

    Made it so you can test header files in c++ be4 in kabe lang

--- compile_program.py
+++ compile_program.py
@@ -4,5 +4,6 @@ from src.headers.templates import packages as pack
 from src.headers.templates import src_path as src
 
 if __name__ == "__main__":
-    system(f"python3 {src}main.py {argv[1]} PAR > {argv[2]}.cpp")
+    if not argv[1] == "COMPILE":
+        system(f"python3 {src}main.py {argv[1]} PAR > {argv[2]}.cpp")
     system(f"g++ -std=c++17 -I{pack} {argv[2]}.cpp -o {argv[2]}.o")
