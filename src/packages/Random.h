7753bc1dbcfd9ed0e6b7aa8e9e97a1048083def8
Kasper <kasper4131@gmail.com>
2021-02-02 09:18:10 +0100

    Test to see if this algorthm works better
    The old fix didn't actually solve the issue of dissimilar rand() functions. This should hopefully work the same on linux and windows.

--- src/packages/Random.h
+++ src/packages/Random.h
@@ -3,41 +3,69 @@
  * easier for cross platform since rand() does not work the
  * same on all systems and also make code with random calls
  * easier to read.
+ * 
+ * The code is written with boost but i believe it can be
+ * written completely with STD lib.
+ * A similar algorithm written completely with STD lib 
+ * can be found here : https://codeforces.com/blog/entry/61587
+ * 
+ * Issues:
+ * I'm unsure if and how seeding works/is working.
+ * This only works with signed integers for now.
+ * 
+ * Usage:
+ * Random r = Random(true/false/uint seed); // or just Random r = Random();
+ * // true = it will update seed every roll (default)
+ * // false = it will run with same seed every roll
+ * // seed = will disable updating seed every roll and set this as seed
+ * int hi = r.roll(0, 1); // will get random number from 0 to 1 including both.
+ * int index = r.roll(vec.size());
+ * // will get random number from index 0 of std::vector called vec
+ * // to last index of vec (i.e. vec.size()-1, since .size includes index 0)
  */
-#pragma once
 #ifndef KABERANDOM
 #define KABERANDOM
-#include <ctime>
-#include <cstdlib>
+
+#include <chrono>
+#include <boost/random.hpp>
 
 namespace kabe {
     class Random {
     private:
-        int _min = 0;
-        int _max = 10;
+        bool alwaysSeed = false;
+        unsigned int currentSeed;
+        boost::random::mt19937 rng = boost::random::mt19937();
     public:
 
-        void setSeed() {
-            srand ( time(NULL) );
-        }
         void setSeed(unsigned int seed) {
-            srand(seed);
+            this->currentSeed = seed; // this should get a copy of seed.
+            rng = boost::random::mt19937(this->currentSeed);
         }
-        Random() {
-            setSeed();
+        void setSeed() {
+            this->setSeed(unsigned(std::chrono::steady_clock::now().time_since_epoch().count()));
         }
-        int roll() { return rand() % _max + _min; }
-        int roll(int min, int max) { return rand() % (max - min) + min; }
-        int roll(int max) { return rand() % max; }
-        void setMinMax(int min, int max) {
-            _min = min;
-            _max = max;
+        unsigned int getSeed() { return currentSeed; }
+        Random(bool _alwaysSeed = true) {
+            this->doAlwaysSeed(_alwaysSeed);
+            this->setSeed();
         }
-        void setMin(int min) {
-            _min = min;
+        Random(unsigned int seed) {
+            this->doAlwaysSeed(false);
+            this->setSeed(seed);
         }
-        void setMax(int max) {
-            _max = max;
+        int roll(int max = 10) { return this->roll(0, max-1); }
+        int roll(int min, int max) {
+            if (this->alwaysSeed) this->setSeed();
+            
+            boost::uniform_int<> range(min, max);
+            boost::variate_generator< boost::random::mt19937, boost::uniform_int<> > dice(this->rng, range);
+            return dice();
+        }
+        void doAlwaysSeed(bool value) {
+            this->alwaysSeed = value;
+        }
+        bool getAlwaysSeed() {
+            return this->alwaysSeed;
         }
     };
 }
